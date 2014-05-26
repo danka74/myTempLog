d6bb90496d02298931671d494a01cff37bf9c471
danka74 <daniel.karlsson@liu.se>
2014-05-26 22:27:23 +0200

    Added database create script, eclipse project file

---
+++ src/sql/create_script.sql
@@ -0,0 +1,8 @@
+DROP TABLE IF EXISTS `temperature`;
+
+CREATE TABLE `temperature` (
+  `timestamp` datetime NOT NULL DEFAULT NOW(),
+  `id` int(11) NOT NULL DEFAULT '0',
+  `value` double DEFAULT NULL,
+  PRIMARY KEY (`timestamp`,`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
