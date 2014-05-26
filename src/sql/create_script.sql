b3cdf6de5f10fef737142fd8748fc90dc7cf0b54
Daniel Karlsson <daniel.karlsson@liu.se>
2014-05-26 22:32:27 +0200

    Update create_script.sql

--- src/sql/create_script.sql
+++ src/sql/create_script.sql
@@ -1,7 +1,7 @@
 DROP TABLE IF EXISTS `temperature`;
 
 CREATE TABLE `temperature` (
-  `timestamp` datetime NOT NULL DEFAULT NOW(),
+  `timestamp` datetime NOT NULL DEFAULT 0,
   `id` int(11) NOT NULL DEFAULT '0',
   `value` double DEFAULT NULL,
   PRIMARY KEY (`timestamp`,`id`)
