86d6b729956ee2df6b92d4335fec70bca8e6139f
Daniel Karlsson <daniel.karlsson@liu.se>
2014-05-27 14:38:11 +0200

    Update store_db.py

--- src/python/store_db.py
+++ src/python/store_db.py
@@ -9,28 +9,38 @@ import sys
 
 import time
 
-tdCore = td.TelldusCore()
+core = td.TelldusCore()
+impossible_value = -300
+prev_values = {}
 
-try:
-    con = mdb.connect('192.168.1.100', 'tempuser', 'App3lKnyckar1azz', 'tempdb');
+while True:
+	
+	sensors = core.sensors()
+	
+	time.sleep(60)
 
-    cur = con.cursor()
+	try:
+	    con = mdb.connect('192.168.1.100', 'tempuser', 'App3lKnyckar1azz', 'tempdb');
+	
+	    cur = con.cursor()
+	
+	    for sensor in sensors:
+		value = sensor.value(const.TELLSTICK_TEMPERATURE)
+		print "{} at {}".format(value.value, time.ctime(value.timestamp))
+		if abs(value.value - prev_values.get(sensor.id, impossible_value)) > 0.5:
+	        	cur.execute("INSERT INTO temperature VALUES (FROM_UNIXTIME(%s), %s, %s);", (value.timestamp, sensor.id, value.value))
+	        	prev_values[sensor.id] = value.value
+	
+	    con.commit()
 
-    for sensor in tdCore.sensors():
-	value = sensor.value(const.TELLSTICK_TEMPERATURE)
-	print "{} at {}".format(value.value, time.ctime(value.timestamp))
-        cur.execute("INSERT INTO temperature VALUES (FROM_UNIXTIME(%s), %s, %s);", (value.timestamp, sensor.id, value.value))
-
-    con.commit()
-    
-except mdb.Error, e:
-
-    con.rollback()
-  
-    print "Error %d: %s" % (e.args[0],e.args[1])
-    sys.exit(1)
-    
-finally:    
-        
-    if con:    
-        con.close()
+	except mdb.Error, e:
+	
+	    con.rollback()
+	  
+	    print "Error %d: %s" % (e.args[0],e.args[1])
+	    sys.exit(1)
+	    
+	finally:    
+	        
+	    if con:    
+	        con.close()
